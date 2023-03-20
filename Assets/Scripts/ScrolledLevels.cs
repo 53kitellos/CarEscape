8864678e59701fc2ac22d6ba1a8c649a7173dcb8
53kitellos <53kitellos@gmail.com>
2023-03-20 15:32:06 +0300

    2023.03.20 - настроил корректное отображение медалей в цент.меню за пройденный уровни/ так же подогнал вертикальный и альбомный формат центрального меню

--- Assets/Scripts/ScrolledLevels.cs
+++ Assets/Scripts/ScrolledLevels.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ScrollButtons : MonoBehaviour
+public class ScrolledLevels : MonoBehaviour
 {
     [SerializeField] private ScriptableObject[] _scriptableObjects;
     [SerializeField] private LevelView _levelView;
